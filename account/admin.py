5c3696ff7533cb0eeea1999b1640704343929f84
GouravSaini125 <rahul777singh000@gmail.com>
2020-01-30 12:11:32 +0530

    backup

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Student,Mentor,Judge,ProblemCreator,User
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username','branch', 'semester',)
+    search_fields = ('=branch', '=semester','user__username')
+
+    def username(self,obj):
+        return obj.user.username
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Mentor)
 admin.site.register(Judge)
 admin.site.register(ProblemCreator)
--- hackApp/admin.py
+++ hackApp/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Problem,Idea
 
+
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('branch', 'semester', 'problem_code',)
+    search_fields = ('=user__branch','=user__semester', '=problem__code',)
+
+    def problem_code(self, instance):
+        return instance.problem.code
+
+    def semester(self, instance):
+        return instance.user.semester
+
+    def branch(self, instance):
+        return instance.user.branch
+
+
+admin.site.register(Idea, IdeaAdmin)
 admin.site.register(Problem)
-admin.site.register(Idea)
